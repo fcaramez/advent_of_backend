d8c97b4fd43921eb9261178f824523c77f7b78ea
fcaramez <francisco.caramez.duarte@gmail.com>
2024-11-18 02:29:35 +0000

    BUN | moves config to another file to avoid overcrowding index.ts

---
+++ bun/src/config/index.ts
@@ -0,0 +1,30 @@
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.all("/ping", (c) => {
+  return c.json({
+    message: "OK",
+    status: 200,
+    success: true,
+  });
+});
+
+app.notFound((c) => {
+  return c.json({
+    message: "Not Found",
+    status: 404,
+    success: false,
+  });
+});
+
+app.onError((err, c) => {
+  console.log(`Error @ ${c.req.url}:`, err);
+  return c.json({
+    message: err.message,
+    status: 500,
+    success: false,
+  });
+});
+
+export default app;
--- bun/src/index.ts
+++ bun/src/index.ts
@@ -1,33 +1,9 @@
 import { Hono } from "hono";
 import users from "./routes/users";
-
+import config from "./config";
 const app = new Hono({ strict: true });
 
-app.all("/ping", (c) => {
-  return c.json({
-    message: "OK",
-    status: 200,
-    success: true,
-  });
-});
-
-app.notFound((c) => {
-  return c.json({
-    message: "Not Found",
-    status: 404,
-    success: false,
-  });
-});
-
-app.onError((err, c) => {
-  console.log(`Error @ ${c.req.url}:`, err);
-  return c.json({
-    message: err.message,
-    status: 500,
-    success: false,
-  });
-});
-
+app.route("/", config);
 app.route("/users", users);
 
 export default {
