95c3f0fff89c1070454aaf93e81df18b03d60a13
fcaramez <francisco.caramez.duarte@gmail.com>
2024-11-18 00:44:49 +0000

    BUN | adds initial ping, onError and notFound routes using Hono

---
+++ bun/src/index.ts
@@ -0,0 +1,33 @@
+import { Hono } from "hono";
+
+const app = new Hono({ strict: true });
+
+app.all("/healthz", (c) => {
+  return c.json({
+    message: "OK",
+    status: 200,
+    success: true,
+  });
+});
+
+app.notFound((c) => {
+  return c.json({
+    message: "Not Found",
+    status: 404,
+    success: false,
+  });
+});
+
+app.onError((err, c) => {
+  console.log(`Error @ ${c.req.url}:`, err);
+  return c.json({
+    message: err.message,
+    status: 500,
+    success: false,
+  });
+});
+
+export default {
+  fetch: app.fetch,
+  port: Bun.env.PORT || 5005,
+};
